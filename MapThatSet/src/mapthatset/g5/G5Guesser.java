af45105e1c90e2d59a3473c0fffe25c88e25a181
Sid Nair <ssn2114@columbia.edu>
2011-10-04 19:34:30 -0400

    style fixes

--- MapThatSet/src/mapthatset/g5/G5Guesser.java
+++ MapThatSet/src/mapthatset/g5/G5Guesser.java
@@ -9,17 +9,17 @@ public class G5Guesser extends Guesser {
 
 	private int mappingLength;
 	private int intLastQueryIndex = 0;
-	private ArrayList<Integer> guess = new ArrayList< Integer >();
+	private ArrayList<Integer> guess = new ArrayList<Integer>();
 		
-	public void startNewMapping( int intMappingLength ) {
+	public void startNewMapping(int intMappingLength) {
 		this.mappingLength = intMappingLength;
 		intLastQueryIndex = 0;
-		guess = new ArrayList< Integer >();
+		guess = new ArrayList<Integer>();
 	}
 	
 	@Override
 	public GuesserAction nextAction() {
-		intLastQueryIndex ++;
+		intLastQueryIndex++;
 		if ( intLastQueryIndex > mappingLength ) {
 			return new GuesserAction("g", guess);
 		} else {
@@ -30,8 +30,7 @@ public class G5Guesser extends Guesser {
 	}
 	
 	@Override
-	public void setResult( ArrayList< Integer > alResult )
-	{
+	public void setResult( ArrayList< Integer > alResult ) {
 		guess.add(alResult.get(0));
 	}
 	
