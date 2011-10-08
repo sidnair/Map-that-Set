ec2a6f7eeaebded7eb309ddea03faaabf4b3441e
Sid Nair <ssn2114@columbia.edu>
2011-10-08 03:42:02 -0400

    add more comments

--- MapThatSet/src/mapthatset/g5/G5Guesser.java
+++ MapThatSet/src/mapthatset/g5/G5Guesser.java
@@ -9,10 +9,9 @@ public class G5Guesser extends Guesser {
 	
 	private final boolean DEBUG = false;
 	private Strategy strategy = new BadPairsStrategy(DEBUG);
-	private RandomLayer randomLayer;
-	
+	private RandomLayer randomLayer;	
 	private int mappingLength;
-		
+
 	public void startNewMapping(int mappingLength) {
 		strategy.startNewMapping(mappingLength);
 		randomLayer = new RandomLayer(mappingLength);
@@ -20,9 +19,19 @@ public class G5Guesser extends Guesser {
 		this.mappingLength = mappingLength;
 	}
 	
+	/**
+	 * Wraps the strategy's next action using the random layer.
+	 */
 	@Override
 	public GuesserAction nextAction() {
 		GuesserAction stratAction = strategy.nextAction();
+		// Don't do any encoding if we're debugging to make debugging a logic
+		// error in a strategy less painful.
+		if (DEBUG) {
+			return stratAction;
+		}
+		// Determine the type of the GuesserAction and decode or encode as
+		// appropriate.
 		if (stratAction.getType() == "g") {
 			return decodeGuess(stratAction);
 		} else {
@@ -30,6 +39,13 @@ public class G5Guesser extends Guesser {
 		}
 	}
 	
+	/**
+	 * Converts an encoded guess to a guess that corresponds with the mapper's
+	 * real scheme.
+	 * 
+	 * @param action the encoded action.
+	 * @return corrected GuesserAction.
+	 */
 	private GuesserAction decodeGuess(GuesserAction action) {
 		Integer[] decodedGuess = new Integer[mappingLength];
 		for (int i = 1; i <= mappingLength; i++) {
@@ -40,6 +56,16 @@ public class G5Guesser extends Guesser {
 					new ArrayList<Integer>(Arrays.asList(decodedGuess)));
 	}
 	
+	/**
+	 * Encodes a query. This makes it more difficult for the mapper from 
+	 * determining if we're following a given strategy. Granted, some strategies
+	 * will always be easy to detect (e.g. the pairwise strategy), but this will
+	 * provide some safety against mappers that optimize for guessers that 
+	 * guess certain numbered patterns.
+	 * 
+	 * @param action the encoded action.
+	 * @return corrected GuesserAction.
+	 */
 	private GuesserAction encodeQuery(GuesserAction action) {
 		ArrayList<Integer> obfuscatedGuess = new ArrayList<Integer>();
 		for (int i : action.getContent()) {
