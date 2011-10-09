8d8c667565449c7bac4cb5ad7c47bf834721b32c
Tao Song <ts2695@columbia.edu>
2011-10-08 20:02:10 -0400

    random layer changed a bit.
    first mapper against the pair and orthogonal guesser, at least better
    than the dumbmapper =.=

--- MapThatSet/src/mapthatset/g5/G5Mapper.java
+++ MapThatSet/src/mapthatset/g5/G5Mapper.java
@@ -3,18 +3,46 @@ package mapthatset.g5;
 import java.util.ArrayList;
 import java.util.Random;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 import mapthatset.sim.GuesserAction;
 import mapthatset.sim.Mapper;
+import mapthatset.g5.RandomLayer;
 
 public class G5Mapper extends Mapper {
+	
+	private int intival = 3;
+	//checked with the pair and orthogonal guesser, when n=3, there will be no orthogonal pair.
 
 	@Override
 	public ArrayList<Integer> startNewMapping(int intMappingLength) {
-		ArrayList<Integer> mapping = new ArrayList<Integer>();
+		ArrayList<Integer> mapping = new ArrayList<Integer>(intMappingLength);
 		Random r = new Random();
-		for (int i = 0; i < intMappingLength; i++) {
-			mapping.add(r.nextInt(intMappingLength) + 1);
+		RandomLayer rl = new RandomLayer(intMappingLength);
+		
+		if(intMappingLength==2){
+			mapping.add(r.nextInt(2)+1);
+			if(r.nextInt(20)!=1){
+				//the Nash Equilibrium is 5% mapping to the same and 95% mapping to different
+				//if we always map to different element, a guesser will ask for one and infer the other.
+				//the penalty of guessing wrong is 10*n, we can compute the Nash Equilibrium
+				//for n>2, computing Nash Equilibrium is hard....
+				mapping.add(mapping.get(0)==1?2:1);
+			}
+			else{
+				mapping.add(mapping.get(0));
+			}
 		}
+		
+		else{
+			int ran = r.nextInt(intival);
+			for (int i = 0; i < intMappingLength; i++) {
+				mapping.add((i+ran)%intival+1);
+			}	
+		}
+		
+		rl.randomize(mapping);
+			
 		return mapping;
 	}
 
--- MapThatSet/src/mapthatset/g5/RandomLayer.java
+++ MapThatSet/src/mapthatset/g5/RandomLayer.java
@@ -5,12 +5,14 @@ import java.util.Random;
 
 public class RandomLayer {
 	private ArrayList<Integer> randomLayer; 
+	private int length;
 	
 	protected RandomLayer(int mappingLength){
+		this.length = mappingLength;
 		Random r = new Random();
 		randomLayer = new ArrayList<Integer>();
 		ArrayList<Integer> left = new ArrayList<Integer>();
-		for(int i=1;i<=mappingLength;i++){
+		for(int i=0;i<mappingLength;i++){
 			left.add(i);
 		}
 		for (int i=0;i<mappingLength;i++){
@@ -23,17 +25,79 @@ public class RandomLayer {
 	protected Integer getMapping(int number) {
 		// The mapping has a 1-based index, and the randomLayer ArrayList has a
 		// 0-based index, so we must correct for that.
-		return randomLayer.get(number - 1);
+		return randomLayer.get(number);
 	}
 	
+	protected Integer indexOf(int number) {
+		return randomLayer.indexOf(number);
+	}
+	
+	
+	//this function randomize the original array, and return the original one.
+	//my original idea of this class was do randomly map array 
+	protected ArrayList<Integer> randomize(ArrayList<Integer> ori){
+		if(ori.size()!=length){
+			System.out.println("wrong");
+			return null;
+		}
+		
+		ArrayList<Integer> original  = new ArrayList<Integer>(ori);
+		for(int i=0;i<length;i++){
+			ori.set(this.getMapping(i),original.get(i));
+		}
+		
+		return original;		
+	}
+	
+	//this function randomize the original array, and return the original one.
+	//my original idea of this class was do randomly map array 
+	protected ArrayList<Integer> deRandomize(ArrayList<Integer> ori){
+			if(ori.size()!=length){
+				System.out.println("wrong");
+				return null;
+			}
+			
+			ArrayList<Integer> original  = new ArrayList<Integer>(ori);
+			for(int i=0;i<length;i++){
+				ori.set(this.indexOf(i),original.get(i));
+			}
+			
+			return original;		
+		}
+	
+	
 	public static void main(String[] args) {
-		int length = 9;
+		int length = 10;
 		RandomLayer rl = new RandomLayer(length);
-		for(int i=1;i<=length;i++){
+		for(int i=0;i<length;i++){
 			System.out.print(rl.getMapping(i));
 			System.out.print(" ");
 		}
 		System.out.println();
+		for(int i=0;i<length;i++){
+			System.out.print(rl.indexOf(i));
+			System.out.print(" ");
+		}
+		System.out.println();
+		ArrayList<Integer> test = new ArrayList<Integer>();
+		
+		for (int i=0;i<length;i++){
+			test.add(i);
+		}
+		rl.randomize(test);
+		for(int i=0;i<length;i++){
+			System.out.print(test.get(i));
+			System.out.print(" ");
+		}
+		System.out.println();
+		
+		rl.deRandomize(test);
+		for(int i=0;i<length;i++){
+			System.out.print(test.get(i));
+			System.out.print(" ");
+		}
+		System.out.println();
+		
 	}
 
 }
